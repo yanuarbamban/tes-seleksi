3a8d48402e509c9ec44b1fc5589c045bb70243c8
yanuarbamban <61087053+yanuarbamban@users.noreply.github.com>
2020-02-15 21:45:41 +0700

    Add files via upload

---
+++ 3 jumlah karakter.php
@@ -0,0 +1,10 @@
+<?php
+$Character= "arka","a";
+$count_character = strlen($);character // result:2 
+echo "countChar : $count_Char";
+
+<?php
+$Characte = "javascript","x";
+$count_character = strlen($karakter); // result: not found
+echo "countChar : $count_Char";
+?>
